4a8f2b59048cb6a236321b7f551bcd6689d584a1
JCampos <jcampos@interfaz.io>
2024-02-26 10:35:06 -0600

    [UPD] Planet Scale Conection

--- Core/Config/Conexion.php
+++ Core/Config/Conexion.php
@@ -30,5 +30,38 @@
 			//Retornar la instancia de conexion
 			return self::$instance[$dbGroup];
 		}//Fin de getConnect
+
+		public static function connectToPlanetScale(){
+			// Use env variables to connect to the database
+			$dsn = "mysql:host={$_ENV["DATABASE_HOST"]};dbname={$_ENV["DATABASE"]}";
+			
+			$options = array(
+				PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/certs/ca-certificates.crt",
+				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+			);
+			
+			$pdo = new PDO($dsn, $_ENV["DATABASE_USERNAME"], $_ENV["DATABASE_PASSWORD"], $options);
+
+			pdo->exec("SET NAMES utf8");
+
+			// Query to fetch list of tables
+			$query = "SHOW TABLES";
+			$stmt = $pdo->query($query);
+			$tables = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
+			if (!empty($tables)) {
+				echo "Tables in the database:\n";
+				foreach ($tables as $table) {
+					echo "- $table\n";
+				}
+			} else {
+				echo "Successfully ran the query. No tables were found in the database.\n";
+			}
+
+			self::$instance[$dbGroup] = pdo;
+
+			return self::$instance[$dbGroup];
+		}
 	}
+	
 ?>
